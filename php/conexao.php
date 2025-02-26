d6a9df2a69f907ae7ab6e703cd7b0a6372c8e0a2
marciodrodrigues <4marciodrodrigues@gmail.com>
2025-02-26 11:12:21 -0300

    Initial commit

---
+++ php/conexao.php
@@ -0,0 +1,12 @@
+<?php
+//Váriaveis 
+
+$url = "localhost";
+$usuario = "root";
+$senha = "";
+$base = "api";
+//Conexão
+
+$conexao = mysqli_connect($url, $usuario, $senha, $base);
+
+?>
---
+++ php/listar.php
@@ -0,0 +1,34 @@
+<?php
+
+//Incluir Conexão
+include("conexao.php");
+
+//Sql
+
+$sql = "SELECT * FROM cursos";
+
+//Executar
+
+$executar = mysqli_query($conexao, $sql);
+
+
+//Vetor
+
+$cursos = [];
+
+//Indice
+
+$indice = 0;
+ 
+//Laço
+while ($linha = mysqli_fetch_assoc($executar)){
+    $cursos[$indice]['idCurso'] = $linha['idCurso'];
+    $cursos[$indice]['nomeCurso'] = $linha['nomeCurso'];
+    $cursos[$indice]['valorCurso'] = $linha['valorCurso'];
+    $indice++;
+}
+//Json
+
+json_encode(['cursos'=>$cursos]);
+
+?>
